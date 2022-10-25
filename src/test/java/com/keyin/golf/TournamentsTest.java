4ca40b879c5f9e4e7338450aa22c0e0a0561b8be
csdouce <chris.doucette@keyin.com>
2022-10-24 21:14:08 -0300

    Initial Commit

--- src/test/java/com/keyin/golf/TournamentsTest.java
+++ src/test/java/com/keyin/golf/TournamentsTest.java
@@ -2,71 +2,30 @@ package com.keyin.golf;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-@ExtendWith(MockitoExtension.class)
 public class TournamentsTest {
-    @Mock
-    private Tournaments mockTournament;
 
     @Test
     public void testCreateNewTournament(){
+        ArrayList<Integer> membersInTourney = new ArrayList<Integer>();
+        membersInTourney.add(122);
+        membersInTourney.add(123);
+        membersInTourney.add(125);
+        membersInTourney.add(126);
+        membersInTourney.add(128);
 
-        ArrayList<Long> membersInTourney = new ArrayList<>();
-        membersInTourney.add(123L);
-        membersInTourney.add(456L);
-        membersInTourney.add(789L);
+        Date startDate = new Date(2022-10-28);
+        Date endDate = new Date(2022-10-31);
 
-        Date startDate = null;
-        try {
-            startDate = new SimpleDateFormat("MMMMM dd, yyyy").parse("October 28, 2022");
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        Tournaments tourney1 = new Tournaments(1, startDate, endDate, "A1 Greengrass Classic", "St. John's Country Club", 250, 1500, membersInTourney);
 
-        Date endDate = null;
-        try{
-            endDate = new SimpleDateFormat("MMMMM dd, yyyy").parse("October 31, 2022");
-        } catch (ParseException e){
-            throw new RuntimeException(e);
-        }
+        Assertions.assertEquals("A1 Greengrass Classic", tourney1.getTournamentName());
+        Assertions.assertNotEquals("St. John's Open", tourney1.getTournamentName());
 
-        Tournaments test1 = new Tournaments(123L, startDate, endDate, "Summerside Open", "Summerside Golf Club", 50L, 1000L, membersInTourney);
-        Tournaments test2 = new Tournaments(456L, startDate, endDate, "PEI Fall Invitational", "Anderson Greek Golf Course", 100L, 2000L, membersInTourney);
-
-        // Testing the creation of Tournament
-        Mockito.when(mockTournament.createNewTournament()).thenReturn(test1);
-        assertEquals(test1, mockTournament.createNewTournament());
-        assertNotEquals(test2, mockTournament.createNewTournament());
-
-        // Testing the get Tournament by ID
-        Mockito.when(mockTournament.getTournamentId()).thenReturn(test1.getTournamentId());
-        assertEquals(test1.getTournamentId(), mockTournament.getTournamentId());
-        assertNotEquals(test2.getTournamentId(), mockTournament.getTournamentId());
-     }
-
-     @Test
-     public void testGetTournamentById() throws ParseException {
-        // Using tournaments.json for testing
-        Tournaments test1 = new Tournaments();
-        Tournaments test2 = new Tournaments();
-
-        test2 = test1.getTournamentById(1);
-
-        Assertions.assertEquals(1L, test2.getTournamentId());
-        Assertions.assertNotEquals(2L, test2.getTournamentId());
+        Assertions.assertEquals("St. John's Country Club", tourney1.getTournamentLocation());
+        Assertions.assertNotEquals("Summerside Golf and Country Club", tourney1.getTournamentLocation());
     }
-
-     }
-
+}
