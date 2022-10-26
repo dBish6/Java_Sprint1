3c5f6a14827eccbacf1dd49afa11f0341ae2c4b4
csdouce <chris.doucette@keyin.com>
2022-10-26 19:33:11 -0300

    Working on getTournamentByID. I am to section where I can create new Tournament Instance and return this instance.
    Also need to sort out changing my Integers to Longs.

--- src/test/java/com/keyin/golf/TournamentsTest.java
+++ src/test/java/com/keyin/golf/TournamentsTest.java
@@ -1,15 +1,24 @@
 package com.keyin.golf;
 
+import com.keyin.golf.json_data.Write;
+import org.json.simple.JSONObject;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.OngoingStubbing;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,30 +29,53 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TournamentsTest {
+    @Mock
+    private Tournaments mockTournament;
 
     @Test
     public void testCreateNewTournament(){
 
-//        test1.createNewTournament();
-//
-//        Assertions.assertEquals("Summerside Open Golf Classic", test1.getTournamentName());
+        ArrayList<Integer> membersInTourney = new ArrayList<>();
+        membersInTourney.add(123);
+        membersInTourney.add(456);
+        membersInTourney.add(789);
 
-//        ArrayList<Integer> membersInTourney = new ArrayList<>();
-//        membersInTourney.add(122);
-//        membersInTourney.add(123);
-//        membersInTourney.add(125);
-//        membersInTourney.add(126);
-//        membersInTourney.add(128);
-//
-//        Date startDate = new Date(2022-10-28);
-//        Date endDate = new Date(2022-10-31);
-//
-//        Tournaments tourney1 = new Tournaments(1, startDate, endDate, "A1 Greengrass Classic", "St. John's Country Club", 250, 1500, membersInTourney);
+        Date startDate = new Date(2022,10-1,28);
+        Date endDate = new Date(2022, 10-1, 31);
 
-//        Assertions.assertEquals("A1 Greengrass Classic", tourney1.getTournamentName());
-//        Assertions.assertNotEquals("St. John's Open", tourney1.getTournamentName());
-//
-//        Assertions.assertEquals("St. John's Country Club", tourney1.getTournamentLocation());
-//        Assertions.assertNotEquals("Summerside Golf and Country Club", tourney1.getTournamentLocation());
-    }
+        Tournaments test1 = new Tournaments(123, startDate, endDate, "Summerside Open", "Summerside Golf Club", 50, 1000, membersInTourney);
+        Tournaments test2 = new Tournaments(456, startDate, endDate, "PEI Fall Invitational", "Anderson Greek Golf Course", 100, 2000, membersInTourney);
+
+        // Testing the creation of Tournament
+        Mockito.when(mockTournament.createNewTournament()).thenReturn(test1);
+        assertEquals(test1, mockTournament.createNewTournament());
+        assertNotEquals(test2, mockTournament.createNewTournament());
+
+        // Testing the get Tournament by ID
+        Mockito.when(mockTournament.getTournamentId()).thenReturn(test1.getTournamentId());
+        assertEquals(test1.getTournamentId(), mockTournament.getTournamentId());
+        assertNotEquals(test2.getTournamentId(), mockTournament.getTournamentId());
+     }
+
+     @Test
+     public void testGetTournamentsById(int tournamentId){
+         ArrayList<Integer> membersInTourney = new ArrayList<>();
+         membersInTourney.add(123);
+         membersInTourney.add(456);
+         membersInTourney.add(789);
+
+         Date startDate = new Date(2022,10-1,28);
+         Date endDate = new Date(2022, 10-1, 31);
+
+         Tournaments test1 = new Tournaments(123, startDate, endDate, "Summerside Open", "Summerside Golf Club", 50, 1000, membersInTourney);
+         Tournaments test2 = new Tournaments(456, startDate, endDate, "PEI Fall Invitational", "Anderson Greek Golf Course", 100, 2000, membersInTourney);
+         Tournaments test3 = new Tournaments();
+
+         // Testing the get Tournament by ID
+         Mockito.when(mockTournament.getTournamentById(123)).thenReturn(test1));
+         assertEquals(test1.getTournamentId(), mockTournament.getTournamentId());
+         assertNotEquals(test2.getTournamentId(), mockTournament.getTournamentId());
+     }
+
+     }
 }
