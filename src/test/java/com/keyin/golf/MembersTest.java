8ebe89b8d75dfe5b5523d5ed53d5ce2fb504523e
BlakeWad1 <100157873+BlakeWad1@users.noreply.github.com>
2022-10-28 11:04:52 -0230

    Changed up members to person class and changing test data

--- src/test/java/com/keyin/golf/MembersTest.java
+++ src/test/java/com/keyin/golf/MembersTest.java
@@ -1,42 +1,24 @@
 package com.keyin.golf;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.ArrayList;
 import java.util.List;
-
-
-
 @ExtendWith(MockitoExtension.class)
 public class MembersTest {
     @Mock
-    private Members mockMember;
-
+    private Person mockMember;
     @Test
-    public void testGetMembersByFirstName(){
-        Members tommy = new Members();
-        tommy.setId(1L);
-        tommy.setFirstName("Tommy");
-
-        Members john = new Members();
-        john.setId(2L);
-        john.setFirstName("John");
-
-        List<Members> membersList = new ArrayList<Members>();
-
-
-        membersList.add(tommy);
-        Mockito.when(mockMember.getFirstName()).thenReturn(tommy.getFirstName());
-        assertEquals(tommy.getFirstName(),mockMember.getFirstName());
-        Mockito.when(mockMember.getFirstName()).thenReturn(john.getFirstName());
-
-
-
+    public void testAddPerson(){
+        Members members = new Members();
+        Long expectedMembershipID = 23L;
+        members.setMembershipID(23L);
+        Assertions.assertEquals(expectedMembershipID,members.getMembershipID());
+        members.addMember(mockMember);
+        Assertions.assertNotNull(members.personList);
     }
-
 }
