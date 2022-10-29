05f600d5b179dfa9d9c25368a756c1964da01af2
BlakeWad1 <100157873+BlakeWad1@users.noreply.github.com>
2022-10-29 11:45:31 -0230

    Fixed MembershipTest date problems.

--- src/test/java/com/keyin/golf/MembershipTest.java
+++ src/test/java/com/keyin/golf/MembershipTest.java
@@ -1,15 +1,22 @@
 package com.keyin.golf;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+
 @ExtendWith(MockitoExtension.class)
 public class MembershipTest {
     @Mock
     private Member mockMember;
+
+
+    //Test to add a new person
     @Test
     public void testAddPerson(){
         Membership membership = new Membership();
@@ -19,4 +26,39 @@ public class MembershipTest {
         membership.addMember(mockMember);
         Assertions.assertNotNull(membership.getMemberList());
     }
+    //Test to test the membership id
+
+
+    @Test
+    public void testMembershipId() {
+
+        Membership membership = new Membership ();
+
+        String result = String.valueOf(membership.getMembershipID());
+
+        Assertions.assertSame(23, result);
+
+        System.out.println("The membership id is suppose to be " + membership.getMembershipID());
+    }
+
+    //Test to find a certain person
+
+    @Test
+    public void testMembershipConstructor() {
+        ArrayList<Member> mockMemberList = new ArrayList<Member>();
+        mockMemberList.add(mockMember);
+        Membership testMembership = new Membership();
+        testMembership.setMembershipID(1L);
+        testMembership.setMembershipStartDate(new Date(2022, 11, 28));
+        testMembership.setMembershipEndDate(new Date(2022, 11, 28));
+        testMembership.setMembershipType("Premium");
+
+
+        Assertions.assertNotNull(testMembership.getMemberList());
+        Assertions.assertEquals(1L, testMembership.getMembershipID());
+        Assertions.assertEquals(new Date(2022, 11, 28), testMembership.getMembershipStartDate());
+        Assertions.assertEquals(new Date(2022, 11, 28), testMembership.getMembershipEndDate());
+        Assertions.assertEquals("Premium", testMembership.getMembershipType());
+    }
+
 }
