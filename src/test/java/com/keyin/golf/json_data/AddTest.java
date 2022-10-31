1be4684063e2f8e44a2e073d0b18e36fb70eb874
Dominic Whelan <dominic.whelan27@gmail.com>
2022-10-31 15:54:33 -0230

    AddTest test added

--- src/test/java/com/keyin/golf/json_data/AddTest.java
+++ src/test/java/com/keyin/golf/json_data/AddTest.java
@@ -1,6 +1,10 @@
 package com.keyin.golf.json_data;
 
+import com.keyin.golf.Member;
+import com.keyin.golf.Tournaments;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class AddTest {
 
@@ -24,6 +28,24 @@ public class AddTest {
 
     @Test
     public void testSetMemberTournamentDetails(){
+
+        int idOfMember2 = 127;
+        Tournaments t = new Tournaments();
+        ArrayList<Long> upcomingTournamentsToEnter = new ArrayList<>();
+        upcomingTournamentsToEnter.add(5L);
+
+        Member memberObject = t.getMemberFromJsonToUpdateTournaments(idOfMember2);
+
+        System.out.println();
+        System.out.println("Member details before update");
+        System.out.println(memberObject.getUpcomingTournaments());
+
+        memberObject.setUpcomingTournaments(upcomingTournamentsToEnter);
+        Add.setMemberTournamentDetails(memberObject);
+
+        System.out.println();
+        System.out.println("Member details after update");
+        System.out.println(t.getMemberFromJsonToUpdateTournaments(idOfMember2));
 
     }
 
